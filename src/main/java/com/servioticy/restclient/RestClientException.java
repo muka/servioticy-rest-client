db99d9dc16b74c5f8aaf2bddfa9a9ec30355cd7d
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2014-02-18 16:02:38 +0100

    es.servioticy -> com.servioticy

--- src/main/java/com/servioticy/restclient/RestClientException.java
+++ src/main/java/com/servioticy/restclient/RestClientException.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/ 
-package es.servioticy.restclient;
+package com.servioticy.restclient;
 
 /**
  * @author Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
